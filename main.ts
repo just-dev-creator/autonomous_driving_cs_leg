a4ad232ed569a30f5f9f5f905c024da0dcd4ae2e
Justus <justus.alvermann@leg-uelzen.de>
2022-12-12 12:50:05 +0100

    Harte Bewegung wegen Tests

--- main.ts
+++ main.ts
@@ -7,17 +7,17 @@ basic.forever(function () {
         motors.motorCommand(MotorCommand.Break)
     }
     if (grove.measureInCentimeters(DigitalPin.C16) < 10) {
-        motors.dualMotorPower(Motor.A, 50)
+        motors.dualMotorPower(Motor.A, 0)
         motors.dualMotorPower(Motor.B, 100)
     }
     if (pins.analogReadPin(AnalogPin.P1) < 50) {
         basic.setLedColor(0xff0000)
-        motors.dualMotorPower(Motor.A, 50)
+        motors.dualMotorPower(Motor.A, 0)
         motors.dualMotorPower(Motor.B, 100)
     } else if (pins.analogReadPin(AnalogPin.P2) < 50) {
         basic.setLedColor(0x00ff00)
         motors.dualMotorPower(Motor.A, 100)
-        motors.dualMotorPower(Motor.B, 50)
+        motors.dualMotorPower(Motor.B, 0)
     } else {
         motors.dualMotorPower(Motor.AB, 75)
     }
